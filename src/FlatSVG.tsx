213943cfa970ac32add06434e5e646fbfe1973fd
Andrew Wei <andrewscwei@gmail.com>
2020-04-09 22:28:10 -0700

    Add FlatSVG component

---
+++ src/FlatSVG.tsx
@@ -0,0 +1,68 @@
+import React, { CSSProperties, PropsWithChildren, SFC } from 'react';
+import styled, { css } from 'styled-components';
+
+interface Props {
+  className?: string;
+  markup: string;
+  style?: CSSProperties;
+  fillColor?: string;
+  strokeColor?: string;
+  isAnimated: boolean;
+}
+
+const FlatSVG: SFC<PropsWithChildren<Props>> = ({ className, style, isAnimated, fillColor, strokeColor, markup }) => (
+  <StyledRoot
+    className={className}
+    dangerouslySetInnerHTML={{ __html: markup }}
+    fillColor={fillColor}
+    isAnimated={isAnimated ?? false}
+    strokeColor={strokeColor}
+    style={style ?? {}}
+  />
+);
+
+export default FlatSVG;
+
+const StyledRoot = styled.figure<{
+  fillColor?: string;
+  strokeColor?: string;
+  isAnimated: boolean;
+}>`
+  box-sizing: border-box;
+  display: inline-block;
+  flex: 0 0 auto;
+  height: 100%;
+  position: relative;
+  width: auto;
+
+  > svg {
+    height: 100%;
+    transition-delay: inherit;
+    transition-duration: inherit;
+    transition-property: inherit;
+    transition-timing-function: inherit;
+    width: auto;
+
+    ${props => props.isAnimated && css`
+      transition-property: stroke, fill, transform;
+      transition-duration: 100ms;
+      transition-timing-function: ease-out;
+      will-change: stroke, fill, transform;
+    `}
+
+    * {
+      transition-delay: inherit;
+      transition-duration: inherit;
+      transition-property: inherit;
+      transition-timing-function: inherit;
+
+      ${props => props.fillColor !== undefined && css`
+        fill: ${props.fillColor};
+        `}
+
+      ${props => props.strokeColor !== undefined && css`
+        stroke: ${props.strokeColor};
+      `}
+    }
+  }
+`;
