31385fb214ca6c715e8b7d027ad6ff82f8c6b85f
Andrew Wei <andrewscwei@gmail.com>
2022-01-13 13:42:03 -0800

    Fix ExtractChildren losing classes assigned to the children

--- src/ExtractChildren.tsx
+++ src/ExtractChildren.tsx
@@ -1,10 +1,14 @@
+import classNames from 'classnames'
 import React, { Children, cloneElement, HTMLAttributes, isValidElement, PropsWithChildren } from 'react'
 
-export default function ExtractChildren({ children, ...props }: PropsWithChildren<HTMLAttributes<HTMLElement>>) {
+export default function ExtractChildren({ children, className, ...props }: PropsWithChildren<HTMLAttributes<HTMLElement>>) {
   return (
     <>
       {Children.map(children, child => {
-        if (isValidElement(child)) return cloneElement(child, { ...props })
+        if (isValidElement(child)) {
+          return cloneElement(child, { className: classNames(className, child.props.className), ...props })
+        }
+
         return child
       })}
     </>
