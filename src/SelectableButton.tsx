39e5ffe0b0e953c25e46175c5491bca595aacb3e
Andrew Wei <andrewscwei@gmail.com>
2022-01-21 15:05:04 -0800

    Add SelectableButton

---
+++ src/SelectableButton.tsx
@@ -0,0 +1,62 @@
+import React, { HTMLAttributes, useEffect, useState } from 'react'
+import styled from 'styled-components'
+
+export type Props = HTMLAttributes<HTMLButtonElement> & {
+  children?: (props: Pick<Props, 'isDeselectable' | 'isDisabled' | 'isSelected' | 'label'>) => JSX.Element
+  isDeselectable?: boolean
+  isDisabled?: boolean
+  isSelected?: boolean
+  label?: string
+  onDeselect?: () => void
+  onSelect?: () => void
+}
+
+export default function SelectableButton({
+  children,
+  isDeselectable = false,
+  isDisabled = false,
+  isSelected: externalIsSelected = false,
+  label,
+  onDeselect,
+  onSelect,
+  ...props
+}: Props) {
+  const [isSelected, setIsSelected] = useState(externalIsSelected)
+
+  const toggleSelection = () => {
+    if (isDisabled) return
+
+    if (isSelected) {
+      if (!isDeselectable) return
+      setIsSelected(false)
+    }
+    else {
+      setIsSelected(true)
+    }
+  }
+
+  useEffect(() => {
+    setIsSelected(externalIsSelected)
+  }, [externalIsSelected])
+
+  useEffect(() => {
+    if (isSelected) {
+      onSelect?.()
+    }
+    else {
+      onDeselect?.()
+    }
+  }, [isSelected])
+
+  return (
+    <StyledRoot {...props} onClick={() => toggleSelection()} disabled={isDisabled || (isSelected && !isDeselectable)}>
+      {children?.({ isDeselectable, isDisabled, isSelected, label }) ?? label}
+    </StyledRoot>
+  )
+}
+
+const StyledRoot = styled.button`
+  &[disabled] {
+    pointer-events: none;
+  }
+`
