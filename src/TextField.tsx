39aea5daa909078e837b5bb4160ee6788a028904
Andrew Wei <andrewscwei@gmail.com>
2022-10-31 21:15:32 -0700

    Remove styled-components TextField

--- src/TextField.tsx
+++ src/TextField.tsx
@@ -1,5 +1,4 @@
 import React, { ChangeEvent, FocusEvent, forwardRef, HTMLAttributes } from 'react'
-import styled from 'styled-components'
 
 export type Props = HTMLAttributes<HTMLInputElement> & {
   value?: string
@@ -15,24 +14,13 @@ export default forwardRef<HTMLInputElement, Props>(({
   onValueChange,
   ...props
 }, ref) => (
-  <StyledRoot
+  <input
     {...props}
     ref={ref}
     type='text'
+    value={value}
+    onBlur={({ target }: FocusEvent<HTMLInputElement>) => onUnfocus?.(target.value)}
     onChange={({ target }: ChangeEvent<HTMLInputElement>) => onValueChange?.(target.value)}
     onFocus={({ target }: FocusEvent<HTMLInputElement>) => onFocus?.(target.value)}
-    onBlur={({ target }: FocusEvent<HTMLInputElement>) => onUnfocus?.(target.value)}
-    value={value}
   />
 ))
-
-const StyledRoot = styled.input`
-  background: #fff;
-  box-sizing: border-box;
-  color: #000;
-  display: block;
-  height: 40px;
-  padding: 0 1rem;
-  text-align: left;
-  width: 100px;
-`
