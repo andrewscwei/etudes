87e4a6ca03161174ab50ee9d50543b48103847be
Andrew Wei <andrewscwei@gmail.com>
2022-11-02 19:24:30 -0700

    Remove lodash

--- src/hooks/useDragEffect.ts
+++ src/hooks/useDragEffect.ts
@@ -1,5 +1,5 @@
+import equal from 'fast-deep-equal'
 import interact from 'interactjs'
-import _ from 'lodash'
 import { DependencyList, Dispatch, RefObject, SetStateAction, useEffect, useRef, useState } from 'react'
 
 // Const debug = process.env.NODE_ENV === 'development' ? require('debug')('etudes:hooks') : () => {}
@@ -14,6 +14,17 @@ type Options<T> = Omit<Parameters<Interact.Interactable['draggable']>[0], 'onsta
    * The initial associated value of this hook.
    */
   initialValue: T
+
+  /**
+   * A function that transforms the drag move delta values to the associated value of this hook.
+   *
+   * @param currentValue - The current associated value.
+   * @param dx - The displacement on the x-axis (in pixels) since the last emitted drag move event.
+   * @param dy - The displacement on the y-axis (in pixels) since the last emitted drag move event.
+   *
+   * @returns The transformed value.
+   */
+  transform?: (currentValue: T, dx: number, dy: number) => T
 
   /**
    * Handler invoked when dragging starts.
@@ -32,17 +43,6 @@ type Options<T> = Omit<Parameters<Interact.Interactable['draggable']>[0], 'onsta
    * Handler invoked when dragging ends.
    */
   onDragEnd?: () => void
-
-  /**
-   * A function that transforms the drag move delta values to the associated value of this hook.
-   *
-   * @param currentValue - The current associated value.
-   * @param dx - The displacement on the x-axis (in pixels) since the last emitted drag move event.
-   * @param dy - The displacement on the y-axis (in pixels) since the last emitted drag move event.
-   *
-   * @returns The transformed value.
-   */
-  transform?: (currentValue: T, dx: number, dy: number) => T
 }
 
 /**
@@ -54,7 +54,14 @@ type Options<T> = Omit<Parameters<Interact.Interactable['draggable']>[0], 'onsta
  *
  * @returns The states created for this effect.
  */
-export default function useDragEffect<T = [number, number]>(targetRef: RefObject<HTMLElement>, { onDragStart, onDragMove, onDragEnd, initialValue, transform, ...options }: Options<T>, deps?: DependencyList): ReturnedStates<T> {
+export default function useDragEffect<T = [number, number]>(targetRef: RefObject<HTMLElement>, {
+  initialValue,
+  transform,
+  onDragStart,
+  onDragMove,
+  onDragEnd,
+  ...options
+}: Options<T>, deps?: DependencyList): ReturnedStates<T> {
   /**
    * Sets the current associated value reference. This reference object is equal to the `value`
    * state but differs slightly in how they are set. Because states are asynchronous by nature, this
@@ -65,7 +72,7 @@ export default function useDragEffect<T = [number, number]>(targetRef: RefObject
    * @returns `true` if the value was set, `false` otherwise.
    */
   const setValueRef = (value: T): boolean => {
-    if (_.isEqual(valueRef.current, value)) return false
+    if (equal(valueRef.current, value)) return false
     valueRef.current = value
 
     return true
