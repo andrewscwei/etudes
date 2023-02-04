f9ce5e835a002637a449087794d5e51531edfe2b
Andrew Wei <andrewscwei@gmail.com>
2023-02-04 11:00:02 -0800

    Add useMounted

---
+++ src/hooks/useMounted.ts
@@ -0,0 +1,15 @@
+import { useEffect, useState } from 'react'
+
+export default function useMounted() {
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+
+    return () => {
+      setIsMounted(false)
+    }
+  }, [])
+
+  return isMounted
+}
