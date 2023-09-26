5ff3f16ce4f2870436571ba980c78e60cee4aef8
Andrew Wei <andrew@sybl.io>
2023-09-26 10:26:41 -0700

    Optimize Collection and Dropdown

--- src/Dropdown.tsx
+++ src/Dropdown.tsx
@@ -10,51 +10,77 @@ import asStyleDict from './utils/asStyleDict'
 import cloneStyledElement from './utils/cloneStyledElement'
 import styles from './utils/styles'
 
+/**
+ * Base extendable type describing the data provided to each item in
+ * {@link Dropdown}.
+ */
 export type DropdownItemData = {
   label?: string
 }
 
+/**
+ * Type describing the props of `ToggleComponent` provided to {@link Dropdown}.
+ */
 export type DropdownToggleProps = HTMLAttributes<HTMLElement> & {
+  /**
+   * Handler invoked to dispatch a custom event.
+   *
+   * @param name User-defined name of the custom event.
+   * @param info User-defined info of the custom event.
+   */
   onCustomEvent?: (name: string, info?: any) => void
 }
 
+/**
+ * Type describing the props of `ItemComponent` provided to {@link Dropdown}.
+ */
 export type DropdownItemProps<T extends DropdownItemData = DropdownItemData> = CollectionItemProps<T>
 
+/**
+ * Type describing the props of {@link Dropdown}.
+ */
 export type DropdownProps<T extends DropdownItemData = DropdownItemData> = HTMLAttributes<HTMLDivElement> & CollectionProps<T> & PropsWithChildren<{
   /**
-   * SVG markup to be put in the dropdown button as the collapse icon.
+   * SVG markup to use as the collapse icon when a toggle button is
+   * automatically generated (when `ToggleComponent` is absent).
    */
   collapseIconSvg?: string
 
   /**
-   * Specifies if the dropdown should be collapsed upon selection. This only
-   * works if `selectionMode` is `single`.
+   * Specifies if the internal collection collapses when an item is selected. This
+   * only works if `selectionMode` is `single`.
    */
   collapsesOnSelect?: boolean
 
   /**
-   * The label to appear on the dropdown toggle button.
+   * The label to appear on the toggle button.
    *
    * @param selection The current selection.
    */
   label?: (selection: CollectionSelection) => string
 
   /**
-   * SVG markup to be put in the dropdown button as the expand icon.
+   * SVG markup to use as the expand icon when a toggle button is automatically
+   * generated (when `ToggleComponent` is absent).
    */
   expandIconSvg?: string
 
   /**
+   * Specifies if the internal collection is collapsed. If this is specified,
+   * the component will not manage expansion state.
+   */
+  isCollapsed?: boolean
+
+  /**
    * Indicates if the component is inverted (i.e. "dropup" instead of dropdown).
-   * Supports all orientations.
    */
   isInverted?: boolean
 
   /**
    * Maximum number of items that are viside when the component expands. When a
    * value greater than or equal to 0 is specified, only that number of items
-   * will be visible at a time, and a scrollbar will appear to scroll to
-   * remaining items. Any value less than 0 indicates that all items will be
+   * will be visible at a time and a scrollbar will appear to enable scrolling
+   * to remaining items. Any value less than 0 indicates that all items will be
    * visible when the component expands.
    */
   maxVisibleItems?: number
@@ -65,24 +91,34 @@ export type DropdownProps<T extends DropdownItemData = DropdownItemData> = HTMLA
   useDefaultStyles?: boolean
 
   /**
+   * Handler invoked when the component is collapsed.
+   */
+  onCollapse?: () => void
+
+  /**
+   * Handler invoked when the component is expanded.
+   */
+  onExpand?: () => void
+
+  /**
    * Handler invoked when the toggle dispatches a custom event.
    *
-   * @param eventName Name of the dispatched custom event.
-   * @param eventInfo Optional info of the dispatched custom event.
+   * @param eventName User-defined name of the dispatched custom event.
+   * @param eventInfo Optional user-defined info of the dispatched custom event.
    */
   onToggleCustomEvent?: (eventName: string, eventInfo?: any) => void
 
   /**
-   * React component type to be used for generating the toggle element inside
-   * the component. When absent, one will be generated automatically.
+   * React component type to be used for generating the toggle button inside the
+   * component. When absent, one will be generated automatically.
    */
   ToggleComponent?: ComponentType<DropdownToggleProps>
 }>
 
 /**
- * A dropdown menu component that is invertible (i.e. can "dropup" instead) and
- * supports both horizontal and vertical orientations. Provide data and item
- * component type to this component to automatically generate menu items.
+ * A dropdown component that is invertible (i.e. can "dropup" instead) and
+ * supports both horizontal and vertical orientations. Provide `items` and
+ * `ItemComponent` props to populate.
  */
 const Dropdown = forwardRef(({
   children,
@@ -90,10 +126,11 @@ const Dropdown = forwardRef(({
   style,
   collapseIconSvg,
   collapsesOnSelect = true,
+  expandIconSvg,
+  isCollapsed: externalIsCollapsed,
+  isInverted = false,
   label,
   layout,
-  expandIconSvg,
-  isInverted = false,
   isSelectionTogglable = false,
   itemLength: externalItemLength,
   itemPadding = 0,
@@ -105,7 +142,9 @@ const Dropdown = forwardRef(({
   selectionMode = 'single',
   useDefaultStyles = false,
   onActivateAt,
+  onCollapse,
   onDeselectAt,
+  onExpand,
   onSelectAt,
   onSelectionChange,
   onToggleCustomEvent,
@@ -114,6 +153,7 @@ const Dropdown = forwardRef(({
   ...props
 }, ref) => {
   const isIndexOutOfRange = (index: number) => {
+    if (isNaN(index)) return true
     if (index >= items.length) return true
     if (index < 0) return true
 
@@ -123,11 +163,25 @@ const Dropdown = forwardRef(({
   const sanitizedSelection = (selection: CollectionSelection) => selection.sort().filter(t => !isIndexOutOfRange(t))
 
   const expand = () => {
-    if (isCollapsed) setIsCollapsed(false)
+    if (!isCollapsed) return
+
+    if (setIsCollapsed) {
+      setIsCollapsed(false)
+    }
+    else {
+      onExpand?.()
+    }
   }
 
   const collapse = () => {
-    if (!isCollapsed) setIsCollapsed(true)
+    if (isCollapsed) return
+
+    if (setIsCollapsed) {
+      setIsCollapsed(true)
+    }
+    else {
+      onCollapse?.()
+    }
   }
 
   const toggle = () => {
@@ -146,64 +200,23 @@ const Dropdown = forwardRef(({
   }
 
   const selectionChangeHandler = (value: CollectionSelection) => {
-    const newValue = value.sort()
-
-    if (isDeepEqual(newValue, selection)) return
-
-    setSelection(newValue)
-  }
-
-  const clickOutsideHandler = (event: MouseEvent) => {
-    if (isCollapsed) return
-    if (!(event.target instanceof Node)) return
-
-    let isOutside = true
-    let node = event.target
-
-    while (node) {
-      if (node === bodyRef.current) {
-        isOutside = false
-        break
-      }
-
-      if (!node.parentNode) break
-
-      node = node.parentNode
+    if (setSelection) {
+      setSelection(value)
     }
-
-    if (!isOutside) return
-
-    collapse()
+    else {
+      onSelectionChange?.(value)
+    }
   }
 
   const bodyRef = useRef<HTMLDivElement>(null)
+  const tracksSelectionChanges = externalSelection === undefined && selectionMode !== 'none'
+  const tracksExpansionChanges = externalIsCollapsed === undefined
+
   const sanitizedExternalSelection = sanitizedSelection(externalSelection)
-  const [selection, setSelection] = useState(sanitizedExternalSelection)
-  const prevSelection = usePrevious(selection, { sanitizeDependency: JSON.stringify })
-  const [isCollapsed, setIsCollapsed] = useState(true)
-  const rect = useElementRect(bodyRef)
+  const [selection, setSelection] = tracksSelectionChanges ? useState(sanitizedExternalSelection) : [sanitizedExternalSelection]
+  const [isCollapsed, setIsCollapsed] = tracksExpansionChanges ? useState(true) : [externalIsCollapsed]
 
-  useEffect(() => {
-    window.addEventListener('click', clickOutsideHandler)
-
-    return () => {
-      window.removeEventListener('click', clickOutsideHandler)
-    }
-  }, [isCollapsed])
-
-  useEffect(() => {
-    if (isDeepEqual(sanitizedExternalSelection, selection)) return
-
-    setSelection(sanitizedExternalSelection)
-  }, [JSON.stringify(sanitizedExternalSelection)])
-
-  useEffect(() => {
-    if (!prevSelection) return
-
-    onSelectionChange?.(selection)
-  }, [JSON.stringify(selection)])
-
-  const itemLength = externalItemLength ?? (orientation === 'vertical' ? rect.height : rect.width)
+  const itemLength = externalItemLength ?? (orientation === 'vertical' ? useElementRect(bodyRef).height : useElementRect(bodyRef).width)
   const numItems = items.length
   const numVisibleItems = maxVisibleItems < 0 ? numItems : Math.min(numItems, maxVisibleItems)
   const menuLength = itemLength * numVisibleItems + itemPadding * (numVisibleItems - 1)
@@ -212,8 +225,77 @@ const Dropdown = forwardRef(({
   const fixedStyles = getFixedStyles({ isCollapsed, isInverted, itemPadding, maxVisibleItems, menuLength, numItems, orientation })
   const defaultStyles: Record<string, any> = useDefaultStyles ? getDefaultStyles({}) : {}
 
-  const expandIconComponent = expandIconSvg ? <FlatSVG svg={expandIconSvg} style={defaultStyles.expandIcon}/> : <></>
-  const collapseIconComponent = collapseIconSvg ? <FlatSVG svg={collapseIconSvg} style={defaultStyles.collapseIcon}/> : expandIconComponent
+  const ExpandIcon = expandIconSvg ? <FlatSVG svg={expandIconSvg} style={defaultStyles.expandIcon}/> : <></>
+  const CollapseIcon = collapseIconSvg ? <FlatSVG svg={collapseIconSvg} style={defaultStyles.collapseIcon}/> : ExpandIcon
+
+  if (setSelection) {
+    const prevSelection = usePrevious(selection, { sanitizeDependency: JSON.stringify })
+
+    useEffect(() => {
+      if (isDeepEqual(sanitizedExternalSelection, selection)) return
+
+      setSelection(sanitizedExternalSelection)
+    }, [JSON.stringify(sanitizedExternalSelection)])
+
+    useEffect(() => {
+      if (prevSelection === undefined) return
+
+      onSelectionChange?.(selection)
+    }, [JSON.stringify(selection)])
+  }
+  else {
+    const prevSelection = usePrevious(sanitizedExternalSelection, { sanitizeDependency: JSON.stringify })
+
+    useEffect(() => {
+      if (!prevSelection) return
+    }, [JSON.stringify(sanitizedExternalSelection)])
+  }
+
+  if (setIsCollapsed) {
+    const prevIsCollapsed = usePrevious(isCollapsed)
+
+    useEffect(() => {
+      if (prevIsCollapsed === undefined) return
+
+      if (isCollapsed) {
+        onCollapse?.()
+      }
+      else {
+        onExpand?.()
+      }
+    }, [isCollapsed])
+  }
+
+  useEffect(() => {
+    const clickOutsideHandler = (event: MouseEvent) => {
+      if (isCollapsed) return
+      if (!(event.target instanceof Node)) return
+
+      let isOutside = true
+      let node = event.target
+
+      while (node) {
+        if (node === bodyRef.current) {
+          isOutside = false
+          break
+        }
+
+        if (!node.parentNode) break
+
+        node = node.parentNode
+      }
+
+      if (!isOutside) return
+
+      collapse()
+    }
+
+    window.addEventListener('click', clickOutsideHandler)
+
+    return () => {
+      window.removeEventListener('click', clickOutsideHandler)
+    }
+  }, [isCollapsed])
 
   return (
     <div
@@ -237,7 +319,7 @@ const Dropdown = forwardRef(({
             onClick={() => toggle()}
           >
             <label style={fixedStyles.toggleLabel} dangerouslySetInnerHTML={{ __html: label?.(selection) ?? (selection.length > 0 ? selection.map(t => items[t].label).join(', ') : '') }}/>
-            {cloneStyledElement(isCollapsed ? expandIconComponent : collapseIconComponent, {
+            {cloneStyledElement(isCollapsed ? ExpandIcon : CollapseIcon, {
               className: classNames(isCollapsed ? fixedClassNames.expandIcon : fixedClassNames.collapseIcon),
               style: styles(isCollapsed ? fixedStyles.expandIcon : fixedStyles.collapseIcon),
             })}
