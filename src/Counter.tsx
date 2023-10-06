fcb3f0d8087c527a39c4870b21a4de8d55c8ff9c
Andrew Wei <andrew@sybl.io>
2023-10-06 13:15:36 -0700

    Add new components

---
+++ src/Counter.tsx
@@ -0,0 +1,168 @@
+import classNames from 'classnames'
+import React, { forwardRef, useEffect, useState, type ComponentType, type HTMLAttributes } from 'react'
+import { TextField } from './TextField'
+import { usePrevious } from './hooks/usePrevious'
+import { asStyleDict } from './utils/asStyleDict'
+import { styles } from './utils/styles'
+
+type Props = Omit<HTMLAttributes<HTMLElement>, 'onChange'> & {
+  min?: number
+  max?: number
+  quantity?: number
+  allowInput?: boolean
+  useDefaultStyles?: boolean
+  onChange?: (quantity: number) => void
+  AddButtonComponent?: ComponentType<HTMLAttributes<HTMLElement>>
+  SubtractButtonComponent?: ComponentType<HTMLAttributes<HTMLElement>>
+}
+
+export const Counter = forwardRef<HTMLDivElement, Props>(({
+  style,
+  min = NaN,
+  max = NaN,
+  allowInput = true,
+  quantity: externalQuantity,
+  useDefaultStyles = true,
+  onChange,
+  AddButtonComponent,
+  SubtractButtonComponent,
+  ...props
+}, ref) => {
+  const handleSubtract = () => {
+    if (setQuantity) {
+      setQuantity(prev => isNaN(min) ? prev - 1 : Math.max(min, prev - 1))
+    }
+    else {
+      onChange?.(isNaN(min) ? quantity - 1 : Math.max(min, quantity - 1))
+    }
+  }
+
+  const handleAdd = () => {
+    if (setQuantity) {
+      setQuantity(prev => isNaN(max) ? prev + 1 : Math.min(max, prev + 1))
+    }
+    else {
+      onChange?.(isNaN(max) ? quantity + 1 : Math.min(max, quantity + 1))
+    }
+  }
+
+  const handleQuantityChange = (newValue: number) => {
+    if (newValue === quantity) return
+
+    if (setQuantity) {
+      setQuantity(newValue)
+    }
+    else {
+      onChange?.(newValue)
+    }
+  }
+
+  const tracksChanges = externalQuantity === undefined
+  const [quantity, setQuantity] = tracksChanges ? useState(1) : [externalQuantity]
+  const prevQuantity = usePrevious(quantity)
+  const defaultStyles = useDefaultStyles ? DEFAULT_STYLES : undefined
+  const isAddingDisabled = !isNaN(max) && quantity + 1 > max
+  const isSubtractingDisabled = !isNaN(min) && quantity - 1 < min
+
+  useEffect(() => {
+    if (prevQuantity === undefined) return
+
+    handleQuantityChange(quantity)
+  }, [quantity])
+
+  useEffect(() => {
+    handleQuantityChange(isNaN(min) ? quantity : Math.max(min, quantity))
+  }, [min])
+
+  useEffect(() => {
+    if (isNaN(max)) return
+
+    handleQuantityChange(isNaN(max) ? quantity : Math.min(max, quantity))
+  }, [max])
+
+  return (
+    <div
+      {...props}
+      data-component='counter'
+      ref={ref}
+      style={styles(style, FIXED_STYLES.root, defaultStyles?.root)}
+    >
+      {SubtractButtonComponent && (
+        <SubtractButtonComponent
+          data-child='subtract-button'
+          className={classNames({ disabled: isSubtractingDisabled })}
+          style={styles(FIXED_STYLES.subtract)}
+          onClick={() => handleSubtract()}
+        />
+      ) || (
+        <button
+          data-child='subtract-button'
+          className={classNames({ disabled: isSubtractingDisabled })}
+          style={styles(FIXED_STYLES.subtract, defaultStyles?.subtract)}
+          onClick={() => handleSubtract()}
+        />
+      )}
+      <TextField
+        data-child='text-field'
+        value={quantity.toString()}
+        isDisabled={!allowInput}
+        formatter={(oldValue, newValue) => isNaN(Number(`0${newValue}`)) ? oldValue : newValue }
+        style={styles(FIXED_STYLES.text, defaultStyles?.text)}
+      />
+      {AddButtonComponent && (
+        <AddButtonComponent
+          data-child='add-button'
+          className={classNames({ disabled: isAddingDisabled })}
+          style={styles(FIXED_STYLES.add)}
+          onClick={() => handleAdd()}
+        />
+      ) || (
+        <button
+          data-child='add-button'
+          className={classNames({ disabled: isAddingDisabled })}
+          style={styles(FIXED_STYLES.add, defaultStyles?.add)}
+          onClick={() => handleAdd()}
+        />
+      )}
+    </div>
+  )
+})
+
+Object.defineProperty(Counter, 'displayName', { value: 'Counter', writable: false })
+
+const FIXED_STYLES = asStyleDict({
+  root: styles({
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'stretch',
+  }),
+  subtract: styles({
+    flex: '0 0 auto',
+  }),
+  add: styles({
+    flex: '0 0 auto',
+  }),
+  text: styles({
+    flex: '1 1 auto',
+  }),
+})
+
+const DEFAULT_STYLES = asStyleDict({
+  root: styles({
+
+  }),
+  subtract: styles({
+    width: '20px',
+    height: '100%',
+    background: '#000',
+  }),
+  add: styles({
+    width: '20px',
+    height: '100%',
+    background: '#000',
+  }),
+  text: styles({
+    height: '100%',
+  }),
+})
---
+++ src/hooks/useScrollPositionEffect.ts
@@ -0,0 +1,58 @@
+import { useEffect, useRef, type DependencyList } from 'react'
+import { Point, Rect } from 'spase'
+
+export type ScrollPositionInfo = {
+  minPos: Point
+  maxPos: Point
+  pos: Point
+  step: Point
+}
+
+type Props = {
+  onChange: (newInfo: ScrollPositionInfo, oldInfo: ScrollPositionInfo | undefined) => void
+}
+
+export function useScrollPositionEffect({ onChange }: Props, deps?: DependencyList) {
+  const handleScrollPositionChange = () => {
+    const newValue = getScrollPositionInfo()
+    if (!newValue) return
+
+    onChange(newValue, prevInfo.current)
+
+    prevInfo.current = newValue
+  }
+
+  const getScrollPositionInfo = (): ScrollPositionInfo | undefined => {
+    const refRect = Rect.fromViewport()
+    const refRectMin = refRect.clone({ x: 0, y: 0 })
+    const refRectFull = Rect.from(window, { overflow: true })
+
+    if (!refRectFull) return undefined
+
+    const refRectMax = refRectMin.clone({ x: refRectFull.width - refRect.width, y: refRectFull.height - refRect.height })
+    const step = new Point([refRect.left / refRectMax.left, refRect.top / refRectMax.top])
+
+    return {
+      minPos: new Point([refRectMin.left, refRectMin.top]),
+      maxPos: new Point([refRectMax.left, refRectMax.top]),
+      pos: new Point([refRect.left, refRect.top]),
+      step,
+    }
+  }
+
+  const prevInfo = useRef<ScrollPositionInfo | undefined>()
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollPositionChange)
+    window.addEventListener('resize', handleScrollPositionChange)
+    window.addEventListener('orientationchange', handleScrollPositionChange)
+
+    handleScrollPositionChange()
+
+    return () => {
+      window.removeEventListener('scroll', handleScrollPositionChange)
+      window.removeEventListener('resize', handleScrollPositionChange)
+      window.removeEventListener('orientationchange', handleScrollPositionChange)
+    }
+  }, [...deps ?? []])
+}
