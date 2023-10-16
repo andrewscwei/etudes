e8d5bac462ea8ec8c4baada4f2b29b0ab4b558a3
Andrew Wei <andrew@sybl.io>
2023-10-16 16:44:38 -0700

    Support responsive images

--- src/Image.tsx
+++ src/Image.tsx
@@ -1,41 +1,69 @@
-import classNames from 'classnames'
 import React, { forwardRef, type HTMLAttributes } from 'react'
 import { type Size } from 'spase'
 import { useLoadImageEffect } from './hooks/useLoadImageEffect'
 import { useDebug } from './utils'
-import { asClassNameDict } from './utils/asClassNameDict'
-import { asStyleDict } from './utils/asStyleDict'
-import { styles } from './utils/styles'
 
 const debug = useDebug('image')
+
+export type ImageSourceDescriptor = {
+  constraints?: {
+    /**
+     * The image's actual width, falls back to the slot width.
+     */
+    width?: number
+
+    /**
+     * The image's slot width associated with the media query satisfying the
+     * specified max width. If the width of the viewport is equal to or less
+     * than this value, the current src will be used. Otherwise, a src defined
+     * for a constraint with a higher width will be used. Falls back to the max
+     * width.
+     */
+    slotWidth?: number
+
+    /**
+     * URL of the image for the current constraint.
+     */
+    src: string
+
+    /**
+     * The max width to use as the media query condition.
+     */
+    maxWidth: number
+  }[]
+
+  /**
+   * Fallback image URL for browsers that do not support the `srcset` attribute.
+   */
+  fallback: string
+}
 
 export type ImageProps = HTMLAttributes<HTMLElement> & {
   alt?: string
   loadingMode?: 'none' | 'lazy' | 'preload'
-  src?: string
+  source: ImageSourceDescriptor
   onImageLoadComplete?: () => void
   onImageLoadError?: () => void
   onImageSizeChange?: (size?: Size) => void
 }
 
 export const Image = forwardRef<HTMLImageElement, ImageProps>(({
-  className,
-  style,
   alt,
   loadingMode = 'preload',
-  src,
+  source,
   onImageLoadComplete,
   onImageLoadError,
   onImageSizeChange,
   ...props
 }, ref) => {
-  const fixedClassNames = getFixedClassNames()
-  const fixedStyles = getFixedStyles()
-
   if (loadingMode === 'preload') {
-    debug('Initiating preload for image...', 'OK', src)
+    debug('Initiating preload for image...', 'OK', source)
 
-    useLoadImageEffect(src, {
+    for (const constraint of source.constraints ?? []) {
+      useLoadImageEffect(constraint.src)
+    }
+
+    useLoadImageEffect(source.fallback, {
       onImageLoadComplete,
       onImageLoadError,
       onImageSizeChange,
@@ -46,27 +74,13 @@ export const Image = forwardRef<HTMLImageElement, ImageProps>(({
     <img
       {...props}
       ref={ref}
-      className={classNames(className, fixedClassNames.root)}
-      style={styles(style, fixedStyles.root)}
-      loading={loadingMode === 'lazy' ? 'lazy' : 'eager'}
-      src={src}
       alt={alt}
+      loading={loadingMode === 'lazy' ? 'lazy' : 'eager'}
+      srcSet={source.constraints?.map(({ slotWidth, maxWidth, width, src }) => `${src} ${width ?? slotWidth ?? maxWidth}w`).join(', ')}
+      sizes={source.constraints?.map(({ maxWidth, slotWidth }) => `(max-width: ${maxWidth}px) ${slotWidth ?? maxWidth}px`).join(', ')}
+      src={source.fallback}
     />
   )
 })
 
 Object.defineProperty(Image, 'displayName', { value: 'Image', writable: false })
-
-function getFixedClassNames() {
-  return asClassNameDict({
-    root: classNames('image'),
-  })
-}
-
-function getFixedStyles() {
-  return asStyleDict({
-    root: {
-      objectFit: 'cover',
-    },
-  })
-}
