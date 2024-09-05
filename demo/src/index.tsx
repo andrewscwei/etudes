a42fc33db2009ce96f28acd8792006fcebdc941c
Andrew Wei <andrewscwei@gmail.com>
2024-09-05 18:21:59 +0900

    refactor: Undo adding TailwindCSS

--- demo/src/index.tsx
+++ demo/src/index.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App.js'
 
-import './styles/global.css'
-
 if (import.meta.env.DEV) {
   window.localStorage.debug = 'demo*,etudes*'
 }
