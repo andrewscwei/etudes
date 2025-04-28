594f42e5e428431208f1eabc8306329eca5998e3
Andrew Wei <andrewscwei@gmail.com>
2025-04-28 10:13:49 -0700

    style: Update tagline

--- demo/src/components/Footer.tsx
+++ demo/src/components/Footer.tsx
@@ -7,7 +7,7 @@ type Props = {
 export function Footer({ className }: Props) {
   return (
     <footer className={clsx(className, 'vb px-safe-or-4 pb-safe-or-8 border-dark/20 text-dark sticky top-0 z-50 flex items-center justify-center border-t pt-8 text-base')}>
-      <span className='text-sm'>a study of styleless React components</span>
+      <span className='text-sm'>a study of headless React components</span>
     </footer>
   )
 }
