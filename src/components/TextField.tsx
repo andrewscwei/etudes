3b763ebd4d9ad4c2ba100d625b9330302790e385
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 10:39:36 -0700

    docs: Add docs for TextField

--- src/components/TextField.tsx
+++ src/components/TextField.tsx
@@ -1,18 +1,69 @@
 import clsx from 'clsx'
 import { forwardRef, useCallback, type ChangeEvent, type FocusEvent, type InputHTMLAttributes } from 'react'
 
+/**
+ * Type describing the props of {@link TextField}.
+ */
 export type TextFieldProps = Omit<InputHTMLAttributes<HTMLInputElement>, 'aria-disabled' | 'aria-placeholder' | 'aria-required' | 'disabled' | 'placeholder' | 'required' | 'type' | 'value' | 'onBlur' | 'onChange' | 'onFocus'> & {
+  /**
+   * The value to set to when the text field is empty.
+   */
   emptyValue?: string
+
+  /**
+   * Specifies if the text field is disabled.
+   */
   isDisabled?: boolean
+
+  /**
+   * Specifies if the text field is required.
+   */
   isRequired?: boolean
+
+  /**
+   * The placeholder text to display when the text field is empty.
+   */
   placeholder?: string
+
+  /**
+   * The value of the text field.
+   */
   value?: string
+
+  /**
+   * A function that formats the value of the text field.
+   *
+   * @param value The value to format.
+   *
+   * @returns The formatted value.
+   */
   formatter?: (value: string) => string
+
+  /**
+   * Handler invoked the text field is focused.
+   *
+   * @param value The value of the text field.
+   */
   onFocus?: (value: string) => void
+
+  /**
+   * Handler invoked the text field is out of focus.
+   *
+   * @param value The value of the text field.
+   */
   onUnfocus?: (value: string) => void
+
+  /**
+   * Handler invoked when the value of the text field changes.
+   *
+   * @param value The new value of the text field.
+   */
   onChange?: (value: string) => void
 }
 
+/**
+ * A text field that supports custom value formatting and empty value.
+ */
 export const TextField = /* #__PURE__ */ forwardRef<HTMLInputElement, Readonly<TextFieldProps>>(({
   className,
   emptyValue = '',
