4fa5c8f94acbeed67f3ec57ac7341151a385f7a9
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 10:23:09 -0700

    docs: Add docs for OptionButton

--- src/components/OptionButton.tsx
+++ src/components/OptionButton.tsx
@@ -1,13 +1,37 @@
 import { forwardRef, type ButtonHTMLAttributes } from 'react'
 
-export type CycleButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'aria-disabled' | 'aria-label' | 'disabled' | 'onChange'> & {
+/**
+ * Type describing the props of {@link OptionButton}.
+ */
+export type OptionButtonProps = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'aria-disabled' | 'aria-label' | 'disabled' | 'onChange'> & {
+  /**
+   * The current index of the option.
+   */
   index?: number
+
+  /**
+   * Specifies if the button is disabled.
+   */
   isDisabled?: boolean
+
+  /**
+   * The list of options to cycle through.
+   */
   options: string[] | { label: string; value: string }[]
+
+  /**
+   * Handler invoked when the button is clicked.
+   *
+   * @param value The value of the next option.
+   * @param index The index of the next option.
+   */
   onChange?: (value: string, index: number) => void
 }
 
-export const OptionButton = /* #__PURE__ */ forwardRef<HTMLButtonElement, Readonly<CycleButtonProps>>(({
+/**
+ * A button component that cycles through a list of options when clicked.
+ */
+export const OptionButton = /* #__PURE__ */ forwardRef<HTMLButtonElement, Readonly<OptionButtonProps>>(({
   index = 0,
   isDisabled = false,
   options,
