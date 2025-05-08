767a2c45859cd5646ac530d95065d0783e1ea33f
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 13:51:28 -0700

    docs: Add docs for useMounted

--- src/hooks/useMounted.ts
+++ src/hooks/useMounted.ts
@@ -1,6 +1,11 @@
 import { useEffect, useState } from 'react'
 
-export function useMounted() {
+/**
+ * Hook for determining if the component is mounted.
+ *
+ * @returns `true` if the component is mounted, `false` otherwise.
+ */
+export function useMounted(): boolean {
   const [isMounted, setIsMounted] = useState(false)
 
   useEffect(() => {
