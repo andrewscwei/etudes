46a286138a41abcc298f5167f3c6e1059fb0af24
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 10:54:17 -0700

    docs: Add docs for useDropzone

--- src/hooks/useDropzone.ts
+++ src/hooks/useDropzone.ts
@@ -1,6 +1,47 @@
 import { useCallback, useState, type DragEvent } from 'react'
 
-export function useDropzone<T = HTMLElement>(action: (file: File) => void) {
+/**
+ * Type describing the output of {@link useDropzone}.
+ */
+export type UseDropzoneOutput<T = HTMLElement> = {
+  /**
+   * Specifies whether the element is currently being dragged over.
+   */
+  isDropping: boolean
+
+  /**
+   * Handler invoked when a file is dragged over the element. This handler
+   * should be added to the `onDragOver` event of the element.
+   *
+   * @param event The drag event.
+   */
+  onDragOver: (event: DragEvent<T>) => void
+
+  /**
+   * Handler invoked when a file is dragged out of the element. This handler
+   * should be added to the `onDragLeave` event of the element.
+   *
+   * @param event The drag event.
+   */
+  onDragLeave: (event: DragEvent<T>) => void
+
+  /**
+   * Handler invoked when a file is dropped on the element. This handler should
+   * be added to the `onDrop` event of the element.
+   *
+   * @param event The drag event.
+   */
+  onDrop: (event: DragEvent<T>) => void
+}
+
+/**
+ * Hook for adding drag-and-drop interaction to an element.
+ *
+ * @param action The handler to call when a file is dropped.
+ *
+ * @returns See {@link UseDropzoneOutput}.
+ */
+export function useDropzone<T = HTMLElement>(action: (file: File) => void): UseDropzoneOutput<T> {
   const [isDropping, setIsDropping] = useState(false)
 
   const onDragOver = useCallback((e: DragEvent<T>) => {
