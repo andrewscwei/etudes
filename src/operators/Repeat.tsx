4265a3d138f5d654ed46cc77d63852ba70226d42
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 16:50:53 -0700

    docs: Add docs for Repeat

--- src/operators/Repeat.tsx
+++ src/operators/Repeat.tsx
@@ -1,14 +1,28 @@
 import { Fragment, type ReactNode } from 'react'
 
+/**
+ * Type describing the properties of {@link Repeat}.
+ */
 export type RepeatProps = {
+  /**
+   * The children to repeat.
+   */
   children?: ReactNode | ((index: number) => ReactNode)
+
+  /**
+   * The number of times to repeat the children.
+   */
   count?: number
+
+  /**
+   * Render function to be called for each index.
+   */
   render?: (index: number) => ReactNode
 }
 
 /**
- * A tag-less component that repeats its children, automatically assigning each
- * a unique key.
+ * A component that repeats its children, automatically assigning each a unique
+ * key.
  */
 export function Repeat({
   count = 1,
