8665a9b8a13cf9772dbf1f5dd90ecfa1e73c35c7
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 17:03:39 -0700

    docs: Add docs for asStyleDict

--- src/utils/asStyleDict.ts
+++ src/utils/asStyleDict.ts
@@ -1,5 +1,13 @@
 import { type CSSProperties } from 'react'
 
+/**
+ * Type-guarding function that ensures the provided dictionary is a valid
+ * dictionary of CSS properties.
+ *
+ * @param dict A dictionary of CSS properties.
+ *
+ * @returns A dictionary of CSS properties.
+ */
 export function asStyleDict<T>(dict: { [K in keyof T]: CSSProperties }) {
   return dict
 }
