47bde59043740c0c521ea4c9cc2e129d0692790c
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 17:03:59 -0700

    docs: Add docs for styles

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,5 +1,13 @@
 import { type CSSProperties } from 'react'
 
+/**
+ * Combines multiple representations of CSS properties into a single object.
+ *
+ * @param args The CSS properties to combine. Each argument can be a
+ *             {@link CSSProperties} object, `undefined`, or `false`.
+ *
+ * @returns A single object containing all the provided CSS properties.
+ */
 export function styles(...args: (CSSProperties | undefined | false)[]): CSSProperties {
   return args.reduce<CSSProperties>((out, curr) => ({
     ...out,
