b754a98d9797358c8be70e527c4a5b2b471ae2e6
Andrew Wei <andrewscwei@gmail.com>
2025-05-08 17:03:25 -0700

    docs: Add docs for asClassNameDict

--- src/utils/asClassNameDict.ts
+++ src/utils/asClassNameDict.ts
@@ -1,3 +1,11 @@
+/**
+ * Type-guarding function that ensures the provided dictionary is a valid
+ * dictionary of class names.
+ *
+ * @param dict A dictionary of class names.
+ *
+ * @returns A dictionary of class names.
+ */
 export function asClassNameDict<T>(dict: { [K in keyof T]: string }) {
   return dict
 }
