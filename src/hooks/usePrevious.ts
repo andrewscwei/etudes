651ae7fe9fab78fd692e4ded7727475c818e2244
Andrew Wei <andrewscwei@gmail.com>
2025-05-12 17:16:47 -0700

    feat: Remove options in usePrevious

--- src/hooks/usePrevious.ts
+++ src/hooks/usePrevious.ts
@@ -1,32 +1,16 @@
 import { useEffect, useRef } from 'react'
 
 /**
- * Type describing the options of {@link usePrevious}.
- */
-export type UsePreviousOptions<T> = {
-  /**
-   * Function to transform the dependency value in the dependency list, useful
-   * if the value is a reference.
-   *
-   * @param dependency The dependency value.
-   *
-   * @returns The transformed value to be used as the dependency value instead.
-   */
-  sanitizeDependency?: (dependency: T) => any
-}
-
-/**
  * Returns the previous value of a value.
  *
  * @param value The value.
- * @param options See {@link UsePreviousOptions}.
  */
-export function usePrevious<T>(value: T, { sanitizeDependency = t => t }: UsePreviousOptions<T> = {}): T | undefined {
+export function usePrevious<T>(value: T): T | undefined {
   const ref = useRef<T>(undefined)
 
   useEffect(() => {
     ref.current = value
-  }, [sanitizeDependency(value)])
+  }, [value])
 
   return ref.current
 }
