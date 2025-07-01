4b2ac0e02bb6166c9815d222474e349266c3f68a
Andrew Wei <andrewscwei@gmail.com>
2025-07-01 15:09:59 -0700

    feat: Update PictureDemo

--- demo/src/demos/PictureDemo.tsx
+++ demo/src/demos/PictureDemo.tsx
@@ -5,21 +5,14 @@ import { Frame } from '../components/Frame.js'
 export function PictureDemo() {
   return (
     <Frame title='Picture'>
-      <Picture
-        className=''
-        sources={[{
-          srcSet: [{
-            src: $$Image,
-            width: 400,
-          }, {
-            src: $$Image,
-            width: 800,
-          }],
-          type: 'image/png',
-          media: '(min-width: 800px)',
-        }]}
-        src={$$Image}
-      />
+      {(_, toast) => (
+        <Picture
+          className='size-full [&_img]:size-full [&_img]:object-cover'
+          src={$$Image}
+          onLoadComplete={() => toast('Loaded')}
+          onLoadStart={() => toast('Loading')}
+        />
+      )}
     </Frame>
   )
 }
