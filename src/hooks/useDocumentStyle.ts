36cccd7dfb56a547f06a93a07a7f5e6e06a88c64
Andrew Wei <andrewscwei@gmail.com>
2025-08-14 07:52:59 -0700

    feat: Add useDocumentStyle and useDocumentClassName

---
+++ src/hooks/useDocumentStyle.ts
@@ -0,0 +1,29 @@
+import { useLayoutEffect } from 'react'
+
+/**
+ * Type describing the options for {@link useDocumentStyle}.
+ */
+type Options = {
+  /**
+   * Specifies whether the hook is enabled, defaults to `true`.
+   */
+  isEnabled?: boolean
+}
+
+/**
+ * Hook to apply a style string to the document's root element.
+ *
+ * @param style The CSS style string to apply to the document's root element.
+ * @param options See {@link Options}.
+ */
+export function useDocumentStyle(style: string, { isEnabled = true }: Options = {}) {
+  useLayoutEffect(() => {
+    if (!isEnabled || typeof window === 'undefined') return
+
+    window.document.documentElement.style = style
+
+    return () => {
+      window.document.documentElement.style = ''
+    }
+  }, [style])
+}
