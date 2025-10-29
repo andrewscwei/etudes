1a61892be609a9a285b8394166c7bb8daaaa8dcc
Andrew Wei <andrewscwei@gmail.com>
2025-10-29 14:17:29 +0900

    fix: Fix function signature of useSetCSSProperty

--- src/hooks/useSetCSSProperty.ts
+++ src/hooks/useSetCSSProperty.ts
@@ -1,18 +1,28 @@
 /**
- * Hook for setting a CSS property value on the window's root element.
+ * Sets a CSS property on the window's root element.
  *
  * @param name Property name to set, e.g., '--primary-color'. Note that the
- *            leading '--' is required for CSS custom properties.
+ *             leading '--' is required for CSS custom properties.
  * @param value Value to set for the CSS property. If `undefined`, the property
  *              will be removed.
  */
-export function useSetCSSProperty(name: string, value?: string) {
-  if (typeof window === 'undefined') return
+type SetCSSProperty = (name: string, value?: string) => void
 
-  if (value === undefined) {
-    window.document.documentElement.style.removeProperty(name)
-  }
-  else {
-    window.document.documentElement.style.setProperty(name, value)
+/**
+ * Hook for returning a function that sets a CSS property value on the window's
+ * root element.
+ *
+ * @returns Function that sets a CSS property on the window's root element.
+ */
+export function useSetCSSProperty(): SetCSSProperty {
+  return (name: string, value?: string) => {
+    if (typeof window === 'undefined') return
+
+    if (value === undefined) {
+      window.document.documentElement.style.removeProperty(name)
+    }
+    else {
+      window.document.documentElement.style.setProperty(name, value)
+    }
   }
 }
