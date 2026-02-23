c139e02591d05de52218e16e5e6f18e696edb2a4
andrewscwei <andrewscwei@users.noreply.github.com>
2026-02-23 17:57:25 +0000

    docs: Update README.md [skip ci]

--- demo/src/components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-export function Footer() {
-  return (
-    <footer className='px-safe pb-safe text-base text-dark'>
-      <div className='flex items-center justify-center px-4 py-8 tab:px-10 note:px-20'>
-        <span className='text-sm'>a study of headless React components</span>
-      </div>
-    </footer>
-  )
-}
